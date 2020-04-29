c942d8afc2a00b7064178c20bcc51c0dd4e5c271
Surya Karanam <iamsurya93@gmail.com>
2020-04-29 11:58:07 +0530

    Updates

---
+++ precache-manifest.50b668f212a9b558f4368e3f043112ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86611f71f169d8230c287eead75d7005",
+    "url": "/surya-portfolio-reactjs/index.html"
+  },
+  {
+    "revision": "9921df1943b5d92d14aa",
+    "url": "/surya-portfolio-reactjs/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "eab057dc8bc158fe11f2",
+    "url": "/surya-portfolio-reactjs/static/css/main.d9ddaad6.chunk.css"
+  },
+  {
+    "revision": "9921df1943b5d92d14aa",
+    "url": "/surya-portfolio-reactjs/static/js/2.0ccbcba1.chunk.js"
+  },
+  {
+    "revision": "63ba80af47535c5571926b75b8de9b89",
+    "url": "/surya-portfolio-reactjs/static/js/2.0ccbcba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab057dc8bc158fe11f2",
+    "url": "/surya-portfolio-reactjs/static/js/main.0cf00fcf.chunk.js"
+  },
+  {
+    "revision": "bb098a1802d770f88f11",
+    "url": "/surya-portfolio-reactjs/static/js/runtime-main.1a9740fe.js"
+  }
+]);
